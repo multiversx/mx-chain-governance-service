b96a4b202b78c17f85f842f4818b6d7afa91997a
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2024-12-18 14:39:25 +0200

    template cleanup

--- libs/services/src/services.module.ts
+++ libs/services/src/services.module.ts
@@ -1,8 +1,4 @@
 import { Global, Module } from '@nestjs/common';
-import { TokenService } from './token/token.service';
-import { UserService } from './user/user.service';
-import { DatabaseModule } from '@libs/database';
-import { ExampleService } from './example/example.service';
 import { DynamicModuleUtils } from '@libs/common';
 import { ViewService } from './view/view.service';
 import { VmQueryModule } from '@libs/common/vm-query/vm-query.module';
@@ -10,20 +6,13 @@ import { VmQueryModule } from '@libs/common/vm-query/vm-query.module';
 @Global()
 @Module({
   imports: [
-    DatabaseModule,
     DynamicModuleUtils.getCachingModule(),
     VmQueryModule,
   ],
   providers: [
-    TokenService,
-    UserService,
-    ExampleService,
     ViewService,
   ],
   exports: [
-    TokenService,
-    UserService,
-    ExampleService,
     ViewService,
   ],
 })
